e361e2e90a2d12fe30e62647cc51253d5d92d942
Kyle Vermeer <kvermeer@stanford.edu>
2013-02-11 02:02:36 -0800

    bad iPad commit

--- StanfordSpot/SSPhotoListViewController.m
+++ StanfordSpot/SSPhotoListViewController.m
@@ -11,7 +11,7 @@
 #import "FlickrFetcher.h"
 #import "SSRecentlyViewedPhotos.h"
 
-@interface SSPhotoListViewController ()
+@interface SSPhotoListViewController () <UISplitViewControllerDelegate>
 
 @end
 
---
+++ StanfordSpot/SSplitViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSplitViewController.h
+//  StanfordSpot
+//
+//  Created by Kyle Vermeer on 2/11/13.
+//  Copyright (c) 2013 Kyle Vermeer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSplitViewController : UISplitViewController
+
+@end
