b9dff97fa48522f2f9e2e955b31c410938aa4902
Kyle Vermeer <kvermeer@stanford.edu>
2013-02-11 11:52:51 -0800

    Revert "Revert "bad iPad commit""
    This reverts commit b89ce199d4f46232a6866db8416670c0eb0ebb98.

---
+++ StanfordSpot/SSDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSDetailViewController.h
+//  StanfordSpot
+//
+//  Created by Kyle Vermeer on 2/11/13.
+//  Copyright (c) 2013 Kyle Vermeer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSDetailViewController : UISplitViewController
+
+@end
